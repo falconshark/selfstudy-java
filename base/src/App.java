a9721d5b328d98ad3783f100274c4fbd85e26856
Sardo <sardo.ip@sardo.work>
2025-02-04 16:15:15 +0900

    Can doing base calculate

--- base/src/App.java
+++ base/src/App.java
@@ -3,28 +3,29 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         //Base math
-        System.out.print(5 + " + " + 2 + " = ");
-        System.out.println(5+2);    //  足し算
-        System.out.print(5 + " - " + 2 + " = ");
-        System.out.println(5-2);    //  引き算
-        System.out.print(5 + " * " + 2 + " = ");
-        System.out.println(5*2);    //  掛算
-        System.out.print(5 + " / " + 2 + " = ");
-        System.out.print(5/2);      //  割り算
-        System.out.print(" 余り ");
-        System.out.println(5%2);    //  剰余
+        int mathAddResult = add(5, 6);
+        System.out.println(mathAddResult);
+        int mathSubtractResult = subtract(10, 6);
+        System.out.println(mathSubtractResult);
+        int mathMultiResult = multi(5, 6);
+        System.out.println(mathMultiResult);
+        int mathDividResult = divid(30, 6);
+        System.out.println(mathDividResult);
     }
-    /*
-     * 
-     * データ型 	説明
-     * boolean 	true or false
-     * char 	16ビットUnicode文字 ¥u0000～¥uFFFF
-     * byte 	8ビット整数 -128～127
-     * short 	16ビット整数 -32768～32767
-     * int 	32ビット整数 -2147483648～2147483647
-     * long 	64ビット整数 -9223372036854775808～9223372036854775807
-     * float 	32ビット単精度浮動小数点数
-     * double 	64ビット倍精度浮動小数点数
-     * 
-     */
+    private static int add(Integer number1, Integer number2) {
+        int result = number1 + number2;
+        return result;
+    }
+    private static int subtract(Integer number1, Integer number2) {
+        int result = number1 - number2;
+        return result;
+    }
+    private static int multi(Integer number1, Integer number2) {
+        int result = number1 * number2;
+        return result;
+    }
+    private static int divid(Integer number1, Integer number2) {
+        int result = number1 / number2;
+        return result;
+    }
 }
