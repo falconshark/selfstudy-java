5fc40fb96fa4d426473022d6e3fb67f9201d6e69
Sardo <sardo.ip@sardo.work>
2025-02-04 15:15:40 +0900

    Try the math and data type of java

--- base/src/App.java
+++ base/src/App.java
@@ -1,5 +1,30 @@
 public class App {
+    // Comment and some code come from https://java.sevendays-study.com/
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //Base math
+        System.out.print(5 + " + " + 2 + " = ");
+        System.out.println(5+2);    //  足し算
+        System.out.print(5 + " - " + 2 + " = ");
+        System.out.println(5-2);    //  引き算
+        System.out.print(5 + " * " + 2 + " = ");
+        System.out.println(5*2);    //  掛算
+        System.out.print(5 + " / " + 2 + " = ");
+        System.out.print(5/2);      //  割り算
+        System.out.print(" 余り ");
+        System.out.println(5%2);    //  剰余
     }
+    /*
+     * 
+     * データ型 	説明
+     * boolean 	true or false
+     * char 	16ビットUnicode文字 ¥u0000～¥uFFFF
+     * byte 	8ビット整数 -128～127
+     * short 	16ビット整数 -32768～32767
+     * int 	32ビット整数 -2147483648～2147483647
+     * long 	64ビット整数 -9223372036854775808～9223372036854775807
+     * float 	32ビット単精度浮動小数点数
+     * double 	64ビット倍精度浮動小数点数
+     * 
+     */
 }
